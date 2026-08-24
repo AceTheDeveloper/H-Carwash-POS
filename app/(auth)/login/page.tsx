aadabd6900468a050b481dce198ceb69120c3397
AceTheDeveloper <ghostwrexker@gmail.com>
2026-08-24 10:16:34 +0800

    Added shadcn login interface

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
 
-function SignIn() {
-  return <div>SignIn</div>;
+import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
+
+import logoImg from "@/assets/h_final_logo.png";
+import frontpageImg from "@/assets/frontpage.jpg";
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-white text-primary-foreground overflow-hidden p-1">
+              <Image
+                alt="H Breakfast to bar"
+                src={logoImg}
+                className="size-full object-contain"
+              />
+            </div>
+            H Breakfast to Bar.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        {/* 3. Pass the imported object directly to the src prop */}
+        <Image
+          src={frontpageImg}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          priority
+        />
+      </div>
+    </div>
+  );
 }
-
-export default SignIn;
