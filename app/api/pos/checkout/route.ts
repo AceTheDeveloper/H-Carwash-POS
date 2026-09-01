cce669ce55fc2da8ce78244e0b636152a0850849
AceTheDeveloper <ghostwrexker@gmail.com>
2026-09-01 15:00:32 +0800

    fix next routes on api/pos/checkout/route.ts

--- app/api/pos/checkout/route.ts
+++ app/api/pos/checkout/route.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
